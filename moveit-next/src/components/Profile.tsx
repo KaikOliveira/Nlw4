e455c3ec0fb830d452e7159b724d19dd663181a9
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-03-01 15:00:42 -0300

    countdownContext

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/KaikOliveira.png" alt="perfil" />
@@ -9,7 +13,7 @@ export function Profile() {
         <strong>Kaik Oliveira</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
